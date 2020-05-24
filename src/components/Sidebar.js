879c653dc1f9583b5de0c0d79373a8b50720c12f
Michael Ignacio <mikelignacio@gmail.com>
2020-05-24 13:43:43 +0800

    Add copy to clipboard on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MobileStoreButton from 'react-mobile-store-button';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const ynabLink = 'https://ynab.com/referral/?ref=X3iIw2rgz_P89RJJ&utm_source=customer_referral';
 const iOSUrl = 'https://apps.apple.com/ph/app/paymaya/id991673877';
@@ -82,6 +83,11 @@ const GcashButton = styled(Button)`
 
 const PayMayaCode = styled.strong`
   font-size: 2.5rem;
+  transition: all .3s;
+
+  &:hover {
+    cursor: pointer;
+  }
 `
 
 const Title = styled.p`
@@ -94,58 +100,67 @@ const DownloadButtons = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  margin-top: 30px;
 `
 
-const Sidebar = () => (
-  <StyledSidebar>
-    <YNAB>
-      <Title>Gain Total Control of Your Money</Title>
-      {/*<p>Stop living paycheck-to-paycheck, get out of debt, and save more money. </p>*/}
-      <p className="mt">
-        <Button
-          href={ynabLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Try YNAB Free
-        </Button>
-      </p>
-    </YNAB>
-    <Gcash>
-      <Title>Try GCash and get P70 worth of freebies</Title>
-      {/*<p>Stop living paycheck-to-paycheck, get out of debt, and save more money. </p>*/}
-      <p className="mt">
-        <GcashButton
-          href={gcashLink}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Claim Gift Now
-        </GcashButton>
-      </p>
-    </Gcash>
-    <PayMaya>
-      <Title>Signup to PayMaya and get P50</Title>
-      <p className="mb-0">Use invite code:</p>
-      <p className="mt-0"><PayMayaCode>bwx3jm</PayMayaCode></p>
-      <DownloadButtons>
-        <MobileStoreButton
-          store="ios"
-          url={iOSUrl}
-          linkProps={{ title: 'Download PayMaya on the App Store' }}
-          height={70}
-          width={200}
-        />
-        <MobileStoreButton
-          store="android"
-          url={androidUrl}
-          linkProps={{ title: 'Download PayMaya on Google Play' }}
-          height={70}
-          width={200}
-        />
-      </DownloadButtons>
-    </PayMaya>
-  </StyledSidebar>
-);
+const Sidebar = () => {
+  const [ isCopied, setIsCopied ] = useState(false);
+
+  return (
+    <StyledSidebar>
+      <YNAB>
+        <Title>Gain Total Control of Your Money</Title>
+        {/*<p>Stop living paycheck-to-paycheck, get out of debt, and save more money. </p>*/}
+        <p className="mt">
+          <Button
+            href={ynabLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Try YNAB Free
+          </Button>
+        </p>
+      </YNAB>
+      <Gcash>
+        <Title>Try GCash and get P70 worth of freebies</Title>
+        {/*<p>Stop living paycheck-to-paycheck, get out of debt, and save more money. </p>*/}
+        <p className="mt">
+          <GcashButton
+            href={gcashLink}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Claim Gift Now
+          </GcashButton>
+        </p>
+      </Gcash>
+      <PayMaya>
+        <Title>Signup to PayMaya and get P50</Title>
+        <CopyToClipboard
+          text='bwx3jm'
+          onCopy={() => setIsCopied(true)}>
+          <PayMayaCode>bwx3jm</PayMayaCode>
+        </CopyToClipboard>
+        {/*<p className="mt-0"><PayMayaCode>bwx3jm</PayMayaCode></p>*/}
+        <DownloadButtons>
+          <MobileStoreButton
+            store="ios"
+            url={iOSUrl}
+            linkProps={{ title: 'Download PayMaya on the App Store' }}
+            height={70}
+            width={200}
+          />
+          <MobileStoreButton
+            store="android"
+            url={androidUrl}
+            linkProps={{ title: 'Download PayMaya on Google Play' }}
+            height={70}
+            width={200}
+          />
+        </DownloadButtons>
+      </PayMaya>
+    </StyledSidebar>
+  );
+}
 
 export default Sidebar;
